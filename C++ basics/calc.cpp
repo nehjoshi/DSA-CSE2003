bf6977a1ed09508e249c77d513a196092e4cf0ad
Neh Joshi <nehjoshi5@gmail.com>
2022-01-04 16:08:10 +0530

    first commit

---
+++ C++ basics/calc.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+bool continueProgram = true;
+
+void askForContinue()
+{
+    char ans;
+    cout << "Do you want to continue? (y/n)" << endl;
+    cin >> ans;
+    if (ans == 'y')
+    {
+        continueProgram = true;
+    }
+    else
+    {
+        continueProgram = false;
+    }
+}
+
+int main()
+{
+    while (continueProgram == true)
+    {
+        double a, b;
+        cout << "Enter two numbers " << endl;
+        cin >> a >> b;
+        char op;
+        cout << "Enter operation" << endl;
+        cin >> op;
+        switch (op)
+        {
+        case '+':
+            cout << "The required sum is: " << a + b << endl;
+            askForContinue();
+            break;
+        case '-':
+            cout << "The required difference is: " << a - b << endl;
+            askForContinue();
+            break;
+        case '*':
+            cout << "The required product is: " << a * b << endl;
+            askForContinue();
+            break;
+        case '/':
+            if (b == 0)
+            {
+                cout << "Division by zero is not allowed" << endl;
+                askForContinue();
+                break;
+            }
+            else
+            {
+                cout << "The required quotient is: " << a / b << endl;
+                askForContinue();
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
