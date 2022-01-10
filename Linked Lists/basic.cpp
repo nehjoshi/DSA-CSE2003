ef9add29777bdc416ca396c458ac2d5f5b37943e
Neh Joshi <nehjoshi5@gmail.com>
2022-01-10 10:41:07 +0530

    Additions

--- Linked Lists/basic.cpp
+++ Linked Lists/basic.cpp
@@ -47,6 +47,21 @@ void AddAtPosition(struct Node **head, int position, int data){  //0 5 10 15 20
     temp->next = n->next;
     n->next = temp;
 }
+void DeleteNode(struct Node **list, int value){
+    struct Node *n = *list;
+    struct Node *temp = new Node;
+    while(n->next->data!=value){
+        n = n->next;
+    }
+    struct Node *prev = n;
+    struct Node *del = n->next;
+    struct Node *after = del->next;
+    temp = after;
+    free(del);
+    n->next = temp;
+
+
+}
 
 int GetLength(struct Node **list){
     int length = 0;
@@ -58,14 +73,30 @@ int GetLength(struct Node **list){
     return length;
 
 }
+int Find(struct Node **list, int value){
+    struct Node *n = *list;
+    int length = GetLength(&n);
+    int position = 1;
+    while(n->data!=value){
+        n = n->next;
+        position++;
+    }
 
+    if (position==0 || position==length){
+        cout << "Element doesn't exist!" << endl;
+        return 0;
+    }
+    else {
+        return position;
+    }
+
+}
 int main()
 {
 
     struct Node *head = new Node;
     head->data = 0;
     head->next = NULL;
-
     Print(head);
     AddAtFront(&head, 12);
     cout << "Now adding 12" << endl;
@@ -81,5 +112,11 @@ int main()
     AddAtPosition(&head, 4, 24);
     Print(head);
     cout << "Length of the linked list: " << GetLength(&head) << endl;
+    DeleteNode(&head, 24);
+    cout << "Deleting 24 from the LL" << endl;
+    Print(head);
+
+    int position = Find(&head, 27);
+    cout << "Position of 13 in the LL is: " << position << endl;
     return 0;
 }
